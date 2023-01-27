064cccbf0f56f7c2013014a12324c7d2819a1139
Mailys <77859157+mailys50@users.noreply.github.com>
2023-01-27 11:50:36 -0300

    tortugas y casas creadas

--- JuegoTortugaUdemy.py
+++ JuegoTortugaUdemy.py
@@ -17,9 +17,24 @@ turtleOne.goto(200,200)
 #ordenar que empice a dibujar
 turtleOne.pendown()
 #ordenamos crear la casa con el circulo
-turtleOne.circle(30)
+turtleOne.circle(40)
+# regrasar la totuga en el sitio de partida dejamos de dibujar
+turtleOne.penup()
+# le damos las coordenadas donde se ubicará la tortuga
+turtleOne.goto(-250,225)
 
 
+turtleTwo.penup()
+# desplazar jugador uno a un lugar especifico
+turtleTwo.goto(200,-200)
+#ordenar que empice a dibujar
+turtleTwo.pendown()
+#ordenamos crear la casa con el circulo
+turtleTwo.circle(40)
+# regrasar la totuga en el sitio de partida dejamos de dibujar
+turtleTwo.penup()
+# le damos las coordenadas donde se ubicará la tortuga
+turtleTwo.goto(-250,-170)
 
 
 
