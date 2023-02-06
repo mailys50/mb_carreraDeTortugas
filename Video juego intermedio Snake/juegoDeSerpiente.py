59382a7a6caf74459144fec80615a07ecb5ba46f
Mailys <77859157+mailys50@users.noreply.github.com>
2023-02-06 18:02:46 -0300

    creación de pantalla y jugador de serpiente

---
+++ Video juego intermedio Snake/juegoDeSerpiente.py
@@ -0,0 +1,15 @@
+import turtle
+#crear pantalla 
+s = turtle.Screen()
+#darle un tamaño a la pantalla que por defecto viene de 500 X 500
+s.setup(650,650)
+# cambiar color
+s.bgcolor('gray')
+s.title('Juego de Serpiente')
+
+#crear jugador
+serpirte  = turtle.Turtle()
+
+
+
+turtle.done()
